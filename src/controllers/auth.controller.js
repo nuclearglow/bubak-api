eaacbde4405eab13789b55f4748145af121dd8c0
Sven Vowe <svenvowe@gmail.com>
2018-10-24 23:17:46 +0200

    using async await for login

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -3,28 +3,28 @@ import logger from '../utils/logging';
 import { createToken } from '../utils/auth';
 
 // login a user
-export const login = (req, res) => {
+export const login = async (req, res) => {
     const { username, password } = req.body;
-    User.findOne({ username })
-        .select('+password')
-        .then((user) => {
-            if (!user) {
-                logger.warn(`Login failed: User not found: ${username}`);
-                res.sendStatus(401);
-            } else {
-                user.verifyPassword(password, (err, valid) => {
-                    if (!err && valid) {
-                        logger.info(`User logged in: ${username}`);
-                        const token = createToken(user.id);
-                        res.status(200).json({ token });
-                    } else {
-                        logger.warn(`Login failed! Password mismatch: ${username}`);
-                        res.sendStatus(401);
-                    }
-                });
-            }
-        })
-        .catch(err => logger.error(`Login failed for ${username}: ${err}`));
+    try {
+        const user = await User.findOne({ username }).select('+password');
+        if (!user) {
+            logger.warn(`Login failed: User not found: ${username}`);
+            res.sendStatus(401);
+        } else {
+            user.verifyPassword(password, (err, valid) => {
+                if (!err && valid) {
+                    logger.info(`User logged in: ${username}`);
+                    const token = createToken(user.id);
+                    res.status(200).json({ token });
+                } else {
+                    logger.warn(`Login failed! Password mismatch: ${username}`);
+                    res.sendStatus(401);
+                }
+            });
+        }
+    } catch (err) {
+        logger.error(`Login failed for ${username}: ${err}`);
+    }
 };
 
 export const recover = (req, res) => {
