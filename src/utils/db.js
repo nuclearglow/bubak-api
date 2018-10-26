e6cefc47a514a57d45868c9932baa42ce341c21b
Sven Vowe <svenvowe@gmail.com>
2018-10-27 00:25:26 +0200

    switched to es6 export and async / await

--- src/utils/db.js
+++ src/utils/db.js
@@ -3,39 +3,41 @@ import mongoose from 'mongoose';
 import config from './config';
 import logger from './logging';
 
-mongoose.Promise = global.Promise;
-const connection = mongoose.connect(config.databaseUri, { useNewUrlParser: true });
-
-connection
-    .then((db) => {
+// only call this function once - when the application starts
+const initializeMoongoose = async () => {
+    try {
+        mongoose.Promise = global.Promise;
+        const db = await mongoose.connect(config.databaseUri, { useNewUrlParser: true });
         logger.info(`Successfully connected to MongoDB cluster in ${config.env} mode.`);
+
+        // If the connection throws an error
+        mongoose.connection.on('error', (err) => {
+            logger.error(`Mongoose default connection error: ${err}`);
+        });
+
+        // When the connection is disconnected
+        mongoose.connection.on('disconnected', () => {
+            logger.warn('Mongoose default connection disconnected');
+        });
+
+        process.on('SIGINT', () => {
+            mongoose.connection.close(() => {
+                logger.warn('Mongoose default connection disconnected through app termination');
+                process.exit(0);
+            });
+        });
+
         return db;
-    })
-    .catch((err) => {
+    } catch (err) {
         if (err.message.code === 'ETIMEDOUT') {
             logger.info('Attempting to re-establish database connection.');
-            mongoose.connect(config.databaseUri);
+            await mongoose.connect(config.databaseUri);
         } else {
             logger.error('Error while attempting to connect to database:');
             logger.error(err);
         }
-    });
+        return null;
+    }
+};
 
-// If the connection throws an error
-mongoose.connection.on('error', (err) => {
-    logger.error(`Mongoose default connection error: ${err}`);
-});
-
-// When the connection is disconnected
-mongoose.connection.on('disconnected', () => {
-    logger.warn('Mongoose default connection disconnected');
-});
-
-process.on('SIGINT', () => {
-    mongoose.connection.close(() => {
-        logger.warn('Mongoose default connection disconnected through app termination');
-        process.exit(0);
-    });
-});
-
-export default connection;
+export default initializeMoongoose;
